5792b3cce683c3b8358b671e1e52a4a8f99ca2f4
surafelfa <surafelfa@gmail.com>
2022-07-29 12:30:34 +0300

    counter

---
+++ React/simpleCounter.jsx
@@ -0,0 +1,34 @@
+class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+      this.increment = this.increment.bind(this);
+      this.decrement = this.decrement.bind(this);
+      this.reset = this.reset.bind(this);
+    }
+    increment(){
+      this.setState(state=>({
+        count: ++state.count
+      }));
+    }
+    decrement(){
+      this.setState(state=>({
+        count: --state.count
+      }));
+    }
+    reset(){
+      this.setState({count: 0});
+    }
+    render() {
+      return (
+        <div>
+          <button className='inc' onClick={this.increment}>Increment!</button>
+          <button className='dec' onClick={this.decrement}>Decrement!</button>
+          <button className='reset' onClick={this.reset}>Reset</button>
+          <h1>Current Count: {this.state.count}</h1>
+        </div>
+      );
+    }
+  };
