4b47241b52548cce2360fd845a7e38399e3ec06a
surafelfa <surafelfa@gmail.com>
2022-07-30 15:27:26 +0300

    plain redux

---
+++ React Redux/.js
@@ -0,0 +1,17 @@
+
+const ADD = 'ADD'
+const addMessage = (message)=>{
+  return {
+    type: ADD,
+    message
+  }
+}
+const messageReducer = (state=[], action)=>{
+  switch(action.type){
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+}
+const store = Redux.createStore(messageReducer);
