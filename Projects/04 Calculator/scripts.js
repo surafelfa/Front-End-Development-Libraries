2d6b91a0f951a44207c0853777476f01ee2e72e6
surafelfa <surafelfa@gmail.com>
2022-08-19 19:33:34 +0300

    calculator -css

--- Projects/04 Calculator/scripts.js
+++ Projects/04 Calculator/scripts.js
@@ -10,19 +10,19 @@ class Calculator extends React.Component{
 
         return (
             <div className="calculator-wrapper">
-                <div className="output label"></div>
+                <div className="output label">&nbsp;</div>
                 <div id="display" className="input label">0</div>
                 <input type="button" value="AC" id="clear" />
-                <input type="button" value="/" id="divide" />
-                <input type="button" value="x" id="multiply" />
+                <input type="button" value="/" id="divide" className="operator"/>
+                <input type="button" value="x" id="multiply"className="operator" />
                 <input type="button" value="7" id="seven" />
                 <input type="button" value="8" id="eight" />
                 <input type="button" value="9" id="nine" />
-                <input type="button" value="-" id="subtract" />
+                <input type="button" value="-" id="subtract" className="operator"/>
                 <input type="button" value="4" id="four" />
                 <input type="button" value="5" id="five" />
                 <input type="button" value="6" id="six" />
-                <input type="button" value="+" id="add" />
+                <input type="button" value="+" id="add" className="operator"/>
                 <input type="button" value="1" id="one" />
                 <input type="button" value="2" id="two" />
                 <input type="button" value="3" id="three" />
