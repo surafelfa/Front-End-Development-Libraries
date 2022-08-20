98414d7ea6ee358984f7b16acc14771acd6eefea
surafelfa <surafelfa@gmail.com>
2022-08-20 07:43:15 +0300

    event handlers

--- Projects/04 Calculator/scripts.js
+++ Projects/04 Calculator/scripts.js
@@ -4,31 +4,121 @@
 class Calculator extends React.Component{
     constructor(props){
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.reset = this.reset.bind(this);
+        this.displayNumber = this.displayNumber.bind(this);
+        this.operatorClicked = this.operatorClicked.bind(this);
+        this.state = {
+            userInput:'0',
+            output:'0',
+        }
     }
-
+    reset(){//resetting the calculator
+        this.setState({
+            userInput: '0',
+            output: '0',
+        });
+    }
+    displayNumber(value){
+        // checking whether a user input is a valid number
+        if(/(^[1-9]|^0\.|^[1-9][0-9]*\.)[0-9]*$/.test(this.state.userInput + value)){
+            this.setState( state => ({
+                userInput: state.userInput + value,
+                output: state.output + value,
+            }));
+        }
+        // accepting the first single digit number
+        else if(this.state.userInput == "0"){
+            this.setState({
+                userInput: value,
+                output: value
+            });
+        }
+        // if there is an operator at last index, change the value 'userInput' with the new one
+        else if("/*-+".includes(this.state.userInput) && value == "."){
+            this.setState( state => ({
+                userInput: "0.",
+                output: state.output + "0.",
+            }));
+        }
+        else if("/*-+".includes(this.state.userInput) && "0123456789".includes(value)){
+            this.setState( state => ({
+                userInput: value,
+                output: state.output + value,
+            }));
+        }       
+    }
+    operatorClicked(value){
+        if(!/[\/\*\-\+]$/.test(this.state.output)){
+            this.setState( state => ({
+                userInput: value,
+                output: state.output + value
+            }));
+        }
+        else{
+            if(value == "-"){
+                this.setState( state => ({
+                    userInput: value,
+                    output: state.output.replace(/[\/\*\+]$/, state.output.charAt(state.output.length-1)+value)
+                }));
+            }
+            else{
+                if(/[\/\*\+]-$/.test(this.state.output)){
+                    this.setState( state => ({
+                        userInput: value,
+                        output: state.output.replace(/[\/\*\+]-$/,value)
+                    }));
+                }
+                else{
+                    this.setState( state => ({
+                        userInput: value,
+                        output: state.output.replace(/[\/\*\-\+]$/,value)
+                    }));
+                }
+            }
+        }
+    }
+    handleClick(event){
+        let value = event.target.value;
+        if(event.target.id === "clear"){
+            this.reset();
+        }
+        else if(".0123456789".includes(value)){
+            this.displayNumber(value)
+        }
+        else if("/*-+".includes(value)){
+            this.operatorClicked(value);            
+        }
+        else if(event.target.id === "equals"){
+            this.setState( state => ({
+                userInput: eval(state.output),
+                output: eval(state.output),
+            }));
+        }
+    }
     render(){
 
         return (
             <div className="calculator-wrapper">
-                <div className="output label">&nbsp;</div>
-                <div id="display" className="input label">0</div>
-                <input type="button" value="AC" id="clear" />
-                <input type="button" value="/" id="divide" className="operator"/>
-                <input type="button" value="x" id="multiply"className="operator" />
-                <input type="button" value="7" id="seven" />
-                <input type="button" value="8" id="eight" />
-                <input type="button" value="9" id="nine" />
-                <input type="button" value="-" id="subtract" className="operator"/>
-                <input type="button" value="4" id="four" />
-                <input type="button" value="5" id="five" />
-                <input type="button" value="6" id="six" />
-                <input type="button" value="+" id="add" className="operator"/>
-                <input type="button" value="1" id="one" />
-                <input type="button" value="2" id="two" />
-                <input type="button" value="3" id="three" />
-                <input type="button" value="0" id="zero" />
-                <input type="button" value="." id="decimal" />
-                <input type="button" value="=" id="equals" />
+                <div className="output label">{this.state.output}</div>
+                <div id="display" className="user-input label">{this.state.userInput}</div>
+                <input type="button" value="AC" id="clear" onClick={this.handleClick}/>
+                <input type="button" value="/" id="divide" onClick={this.handleClick} className="operator"/>
+                <input type="button" value="*" id="multiply" onClick={this.handleClick} className="operator" />
+                <input type="button" value="7" id="seven" onClick={this.handleClick} />
+                <input type="button" value="8" id="eight" onClick={this.handleClick} />
+                <input type="button" value="9" id="nine" onClick={this.handleClick} />
+                <input type="button" value="-" id="subtract" onClick={this.handleClick} className="operator"/>
+                <input type="button" value="4" id="four" onClick={this.handleClick} />
+                <input type="button" value="5" id="five" onClick={this.handleClick} />
+                <input type="button" value="6" id="six" onClick={this.handleClick} />
+                <input type="button" value="+" id="add" onClick={this.handleClick} className="operator"/>
+                <input type="button" value="1" id="one" onClick={this.handleClick} />
+                <input type="button" value="2" id="two" onClick={this.handleClick} />
+                <input type="button" value="3" id="three" onClick={this.handleClick} />
+                <input type="button" value="0" id="zero" onClick={this.handleClick} />
+                <input type="button" value="." id="decimal" onClick={this.handleClick} />
+                <input type="button" value="=" id="equals" onClick={this.handleClick} />
             </div>
         );
     }
