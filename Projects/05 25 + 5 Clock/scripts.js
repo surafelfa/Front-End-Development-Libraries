e0f73cddfdb8d41c50cace580bb48febc9266c53
surafelfa <surafelfa@gmail.com>
2022-09-01 10:13:24 +0300

    handling events for down/up arrows clicked

--- Projects/05 25 + 5 Clock/scripts.js
+++ Projects/05 25 + 5 Clock/scripts.js
@@ -7,7 +7,69 @@ class Clock extends React.Component{
         this.state = {
             breakLenght: 5,
             sessionLength: 25,
+            mm: 25,
         }
+        this.breakDecrementClicked =  this.breakDecrementClicked.bind(this);
+        this.breakIncrementClicked = this.breakIncrementClicked.bind(this);
+        this.sessionDecrementClicked = this.sessionDecrementClicked.bind(this);
+        this.sessionIncrementClicked = this.sessionIncrementClicked.bind(this);
+        this.startTimer_mm = this.startTimer_mm.bind(this);
+        this.startTimer_ss = this.startTimer_ss.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+    breakDecrementClicked(){
+        if(this.state.breakLenght > 1){
+            this.setState( state =>({
+                breakLenght: --state.breakLenght,
+            }));
+        }
+    }
+    breakIncrementClicked(){
+        if(this.state.breakLenght < 5){
+            this.setState( state =>({
+                breakLenght: ++state.breakLenght,
+            }));
+        }
+    }
+    sessionDecrementClicked(){
+        if(this.state.sessionLength > 1){
+            this.setState( state =>({
+                sessionLength: --state.sessionLength,
+                mm: --state.mm,
+            }));
+        }
+    }
+    sessionIncrementClicked(){
+        if(this.state.sessionLength < 60){
+            this.setState( state =>({
+                sessionLength: ++state.sessionLength,
+                mm: ++state.mm,
+            }));
+        }
+    }
+    startTimer_ss(ss){
+        let ssCounter = setInterval(()=>{
+            --ss;
+            if(ss == 0) clearInterval(ssCounter);
+            if(second > 9) document.getElementById('ss').innerHTML = ss;
+            else document.getElementById('ss').innerHTML = `0${ss}`;
+        }, 1000);
+    }
+    startTimer_mm(mm){
+        let mmCounter = setInterval(()=>{
+            --mm;
+            if(mm == 0) clearInterval(mmCounter);
+            if(mm > 9) document.getElementById('mm').innerHTML = mm;
+            else document.getElementById('mm').innerHTML = `0${mm}`;
+                
+            }, 60000);
+    }
+    reset(){
+        this.setState({
+            breakLenght: 5,
+            sessionLength: 25,
+            mm: 25,
+        })
     }
     render(){
 
@@ -19,29 +81,29 @@ class Clock extends React.Component{
                         <div id="break-label">
                             Break Length
                         </div>
-                        <i id="break-decrement" className="fas fa-arrow-down"></i>
+                        <i id="break-decrement" className="fas fa-arrow-down" onClick={this.breakDecrementClicked}></i>
                         <span id="break-length"> {this.state.breakLenght} </span>
-                        <i id="break-increment" className="fas fa-arrow-up"></i>
+                        <i id="break-increment" className="fas fa-arrow-up" onClick={this.breakIncrementClicked}></i>
                     </span>
                     <span className="spacer">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span className="session-wrapper">
                         <div id="session-label">
                             Session Length
                         </div>
-                        <i id="session-decrement" className="fas fa-arrow-down"></i>
+                        <i id="session-decrement" className="fas fa-arrow-down" onClick={this.sessionDecrementClicked}></i>
                         <span id="session-length"> {this.state.sessionLength} </span>
-                        <i id="session-increment" className="fas fa-arrow-up"></i>
+                        <i id="session-increment" className="fas fa-arrow-up" onClick={this.sessionIncrementClicked}></i>
                     </span>
                 </div>
                 <div className="timer">
                     <p id="timer-label">Session</p>
                     <span id="time-left">
-                        25:00
+                        <span id="mm">{this.state.mm}</span>:<span id="ss">00</span>
                     </span>                   
                 </div>
                 <div className="clickable">
                     <button id="start_stop"><i className="fa fa-play"></i><i className="fa fa-pause"></i></button>
-                    <button id="reset"><i className="fa fa-arrows-rotate"></i></button>
+                    <button id="reset" onClick={this.reset}><i className="fa fa-arrows-rotate"></i></button>
                 </div>
             </div>
         );
